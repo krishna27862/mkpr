fef2b7747c20523a4a29f0f63b7c36bf4fe4b8be
Krishna <krishnakanthpatnaik@outlook.com>
2018-06-13 00:48:17 +0530

    JS Script Changes

--- core/src/main/java/com/aem/mkpr/core/components/DynamicBackground.java
+++ core/src/main/java/com/aem/mkpr/core/components/DynamicBackground.java
@@ -1,5 +1,22 @@
 package com.aem.mkpr.core.components;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Model(adaptables=Resource.class)
 
 public class DynamicBackground {
+    @Inject @Named("backgroundstyle") @Default(values = "Select")
+    private String backgroundstyle;
+
+    public String getBackgroundstyle() {
+        return backgroundstyle;
+    }
+
+
 }
+
--- ui.apps/src/main/content/jcr_root/apps/mkpr/components/content/dynamic/dynamicLibs/scripts/script.js
+++ ui.apps/src/main/content/jcr_root/apps/mkpr/components/content/dynamic/dynamicLibs/scripts/script.js
@@ -1,20 +1,21 @@
-function dynamicCheck(PROPERTIES) {
-    var VALUE = $(PROPERTIES.PARENT_CLASS).children("input")[0].value;
-    if ( VALUE == PROPERTIES.IMAGE) {
-        $(PROPERTIES.IMAGE_CLASS).show();
-        $(PROPERTIES.COLOR_CLASS).hide();
-    } else if (VALUE == PROPERTIES.COLOR) {
-        $(PROPERTIES.IMAGE_CLASS).hide();
-        $(PROPERTIES.COLOR_CLASS).show();
+/*(function($, $document) {
+"use strict";
 
-    } else{
-        $(PROPERTIES.IMAGE_CLASS+ "," +PROPERTIES.COLOR_CLASS).hide();
-    }
-}
+$document.on("dialog-ready", function() {
+var PROPERTIES = {
+IMAGE: "image",
+COLOR: "color",
+PARENT_CLASS: ".dynamicstyle",
+IMAGE_CLASS:".backgroundimage",
+COLOR_CLASS:".backgroundcolor"
+};
+dynamicCheck(PROPERTIES);
+$(PROPERTIES.PARENT_CLASS).change(function(){
+dynamicCheck(PROPERTIES)
+});
+});
 
-
-(function($, $document) {
-    "use strict";
+})($, $(document));*/
 var PROPERTIES = {
     IMAGE: "image",
     COLOR: "color",
@@ -22,12 +23,25 @@ var PROPERTIES = {
     IMAGE_CLASS:".backgroundimage",
     COLOR_CLASS:".backgroundcolor"
 };
-    $document.on("dialog-ready", function() {
+$(document).on("foundation-contentloaded", function(){
+
         dynamicCheck(PROPERTIES);
-        $(PROPERTIES.PARENT_CLASS).change(function(){
+       	$(PROPERTIES.PARENT_CLASS).change(function(){
         dynamicCheck(PROPERTIES)
-        });
     });
 
-})($, $(document));
-
+});
+function dynamicCheck(PROPERTIES) {
+    var VALUE = $(PROPERTIES.PARENT_CLASS).children("input")[0].value;
+    
+    if ( $(PROPERTIES.PARENT_CLASS).children("input")[0].value == PROPERTIES.IMAGE) {
+        $(PROPERTIES.IMAGE_CLASS).show();
+        $(PROPERTIES.COLOR_CLASS).hide();
+    } else if ($(PROPERTIES.PARENT_CLASS).children("input")[0].value == PROPERTIES.COLOR) {
+        $(PROPERTIES.IMAGE_CLASS).hide();
+        $(PROPERTIES.COLOR_CLASS).show();
+        
+    } else{
+        $(PROPERTIES.IMAGE_CLASS+ "," +PROPERTIES.COLOR_CLASS).hide();
+    }
+}
