013c69d2cde3cb8b831a68050aa8a2b769edbd5e
Krishnakanth <krishnakanthpatnaik@outlook.com>
2018-06-13 19:25:54 +0530

    Same components at once

--- ui.apps/src/main/content/jcr_root/apps/mkpr/components/content/dynamic/dynamicLibs/scripts/script.js
+++ ui.apps/src/main/content/jcr_root/apps/mkpr/components/content/dynamic/dynamicLibs/scripts/script.js
@@ -1,47 +1,43 @@
-/*(function($, $document) {
-"use strict";
+(function($){
+    var LAST_VALUE = "empty";
 
-$document.on("dialog-ready", function() {
-var PROPERTIES = {
-IMAGE: "image",
-COLOR: "color",
-PARENT_CLASS: ".dynamicstyle",
-IMAGE_CLASS:".backgroundimage",
-COLOR_CLASS:".backgroundcolor"
-};
-dynamicCheck(PROPERTIES);
-$(PROPERTIES.PARENT_CLASS).change(function(){
-dynamicCheck(PROPERTIES)
-});
-});
+    var PROPERTIES = {
+        IMAGE: "image",
+        COLOR: "color",
+        PARENT_CLASS: ".dynamicstyle",
+        IMAGE_CLASS:".backgroundimage",
+        COLOR_CLASS:".backgroundcolor"
+    };
 
-})($, $(document));*/
-var PROPERTIES = {
-    IMAGE: "image",
-    COLOR: "color",
-    PARENT_CLASS: ".dynamicstyle",
-    IMAGE_CLASS:".backgroundimage",
-    COLOR_CLASS:".backgroundcolor"
-};
-$(document).on("foundation-contentloaded", function(){
 
-        dynamicCheck(PROPERTIES);
-       	$(PROPERTIES.PARENT_CLASS).change(function(){
-        dynamicCheck(PROPERTIES)
-    });
+        $(document).on("foundation-contentloaded", function(){
+                LAST_VALUE = currentNodeCheck();
+                LAST_VALUE = dynamicCheck();
+                $(PROPERTIES.PARENT_CLASS).change(function(){
+                LAST_VALUE = dynamicCheck();
+           });
+        });
+    function currentNodeCheck(){
+          $("div[datanodeid]").children("div#"+LAST_VALUE, function(){
+                if(this.attr() == ''){
+                return this.attr("id");
+                }
+          });
+         }
 
-});
-function dynamicCheck(PROPERTIES) {
-    var VALUE = $(PROPERTIES.PARENT_CLASS).children("input")[0].value;
-    
-    if ( $(PROPERTIES.PARENT_CLASS).children("input")[0].value == PROPERTIES.IMAGE) {
-        $(PROPERTIES.IMAGE_CLASS).show();
-        $(PROPERTIES.COLOR_CLASS).hide();
-    } else if ($(PROPERTIES.PARENT_CLASS).children("input")[0].value == PROPERTIES.COLOR) {
-        $(PROPERTIES.IMAGE_CLASS).hide();
-        $(PROPERTIES.COLOR_CLASS).show();
-        
-    } else{
-        $(PROPERTIES.IMAGE_CLASS+ "," +PROPERTIES.COLOR_CLASS).hide();
+    function dynamicCheck() {
+        var VALUE = $(PROPERTIES.PARENT_CLASS+" input[name='./backgroundstyle']").val() || LAST_VALUE;
+        if (VALUE == PROPERTIES.IMAGE ) {
+            $(PROPERTIES.IMAGE_CLASS).show();
+            $(PROPERTIES.COLOR_CLASS).hide();
+        } else if (VALUE == PROPERTIES.COLOR) {
+            $(PROPERTIES.IMAGE_CLASS).hide();
+            $(PROPERTIES.COLOR_CLASS).show();
+
+        } else{
+            $(PROPERTIES.IMAGE_CLASS+ "," +PROPERTIES.COLOR_CLASS).hide();
+        }
+        return VALUE;
     }
-}
+
+})(Granite.$);
